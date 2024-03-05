3ed138416e4c7fa7d9c658e57f6961be151d215a
Shubhankar Kaushik <shubhankar.kaushik2003@gmail.com>
2024-03-06 00:45:03 +0530

    Improved Code

--- File_Renamer.py
+++ File_Renamer.py
@@ -4,13 +4,20 @@ import re
 
 root_folder = Path('C:/Series/New Girl')
 
-for folder_name, _ , _ in os.walk(root_folder):
+renamed = False
+
+for folder_name, sub_folder, file_name in os.walk(root_folder):
     folder_path = Path(folder_name)
     match = re.search(r'Season\s+(\d+)', folder_name)
     if match:
         season_number = match.group(1)
         new_folder_name = f"Season {season_number}"
         new_folder_path = folder_path.parent / new_folder_name
-        folder_path.rename(new_folder_path)
-    
-print ('Renaming Process Completed')
+        if folder_path != new_folder_path:
+            folder_path.rename(new_folder_path)
+            renamed = True
+
+if renamed:
+    print('Renaming process completed.')
+else:
+    print('Folders are already renamed.')
